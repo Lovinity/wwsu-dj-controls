6a3d250763ddf5a5f74290ba8a9d069872be61d4
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-14 18:52:28 -0400

    That was too aggressive considering other silence detections

--- peer.js
+++ peer.js
@@ -554,8 +554,8 @@ function onReceiveStream (stream) {
 
                           // Dead Silence because of networking issue
                           } else if (maxVolume < 0.001) {
-                            window.peerError += 6
-                            window.peerGoodBitrate -= 6
+                            window.peerError += 5
+                            window.peerGoodBitrate -= 5
                             checkPeerError()
                             // Connection was good in the last second. Lower any error counters and also increase the good bitrate counter
                           } else {
