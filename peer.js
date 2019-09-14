dd4386325cd36914710962658a4e28a41b03a71d
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-14 18:50:56 -0400

    make silence more aggressive

--- peer.js
+++ peer.js
@@ -554,8 +554,8 @@ function onReceiveStream (stream) {
 
                           // Dead Silence because of networking issue
                           } else if (maxVolume < 0.001) {
-                            window.peerError += 4
-                            window.peerGoodBitrate -= 4
+                            window.peerError += 6
+                            window.peerGoodBitrate -= 6
                             checkPeerError()
                             // Connection was good in the last second. Lower any error counters and also increase the good bitrate counter
                           } else {
